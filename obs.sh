7015789130b2f0ab7e1c47c634bb843b67f3c56e
Khary Mendez <kmendez@redhat.com>
2022-06-14 08:58:03 -0400

    obs updates

--- obs.sh
+++ obs.sh
@@ -2,9 +2,14 @@
 pidof obs
 if [ $? != 0 ] 
    then
-      nohup /usr/bin/obs --startvirtualcam --studio-mode --scene "Camera" &
-      # --minimize-to-tray
-      sleep 2
+      nohup /usr/bin/obs --startvirtualcam --scene "Camera" &
+      sleep 3
+fi
+
+xdotool search --name "^Display Webcam .*$"
+if [ $? != 0 ]
+   then
       nohup python3 -m http.server -d /home/kmendez/Projects/local/webcam-browser 8087 &
+      #chromium-browser --new-window --profile-directory="Profile 1" --enable-features=OverlayScrollbar --app=http://localhost:8087
       vivaldi --new-window --profile-directory="Profile 1" --enable-features=OverlayScrollbar --app=http://localhost:8087
 fi
--- obs_scene.sh
+++ obs_scene.sh
@@ -6,12 +6,14 @@ if [ $1 -ge 3 ]
     /home/kmendez/.local/bin/lc on
 fi
 
-for i in {0..10}
-do
-  xdotool search --desktop $i --name '^OBS .+$' windowactivate --sync key shift+$1 ctrl+t
-  if [ $? -eq 0 ]
-    then
-      echo "Found it at desktop $i"
-      exit 0
-  fi
-done
+xdotool search --name '^OBS .+$' windowactivate --sync key alt+shift+$1 
+
+#for i in {0..10}
+#do
+#  xdotool search --desktop $i --name '^OBS .+$' windowactivate --sync key alt+shift+$1 
+#  if [ $? -eq 0 ]
+#    then
+#      echo "Found it at desktop $i"
+#      exit 0
+#  fi
+#done
