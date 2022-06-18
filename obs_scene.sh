3280a0b3fbcf15d17c93f2ba8b99479545596c28
Khary Mendez <kmendez@redhat.com>
2022-06-17 20:44:52 -0400

    Fri Jun 17 08:44:52 PM EDT 2022

--- obs_scene.sh
+++ obs_scene.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-if [ $1 -ge 3 ] 
-  then
-    /home/kmendez/.local/bin/lc off
-  else
-    /home/kmendez/.local/bin/lc on
-fi
-
 for i in {0..10}
 do
   xdotool search --desktop $i --name '^OBS .+$' windowactivate --sync key alt+shift+$1 
