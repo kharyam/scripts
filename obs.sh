c508b9ce5419fe6268afcfaf6b92e0a4e25317e5
Khary Mendez <kmendez@redhat.com>
2022-06-20 01:52:17 -0400

    Mon Jun 20 01:52:17 AM EDT 2022

--- obs.sh
+++ obs.sh
@@ -2,6 +2,7 @@
 pidof obs
 if [ $? != 0 ] 
    then
+      export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib 
       nohup /usr/local/bin/obs --startvirtualcam --scene "Camera" &
       #nohup flatpak run com.obsproject.Studio --startvirtualcam --scene "Camera" &
       sleep 3
