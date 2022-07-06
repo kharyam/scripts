2c7c72cc181f8d49d8ca90c35624eef3949efc6a
Khary Mendez <kmendez@redhat.com>
2022-07-05 23:10:05 -0400

    Tue Jul  5 11:10:05 PM EDT 2022

--- noisecorrect.sh
+++ noisecorrect.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-NOISETORCH="NoiseTorch Microphone for Virtual_Mic"
+NOISETORCH="NoiseTorch Microphone for Cam Link 4K Digital Stereo (IEC958)"
+SOURCE="alsa_input.usb-Elgato_Cam_Link_4K_000712A007000-03.iec958-stereo"
 
 # Enable noise torch on virtual mic
 noisetorch -u
-noisetorch -i -s Virtual_Mic
+noisetorch -i -s $SOURCE
 pactl set-default-source "$NOISETORCH"
