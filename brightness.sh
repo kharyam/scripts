ea7233157b424efcc0e8b0bfb418b6635d478b96
Khary Mendez <kharyam@gmail.com>
2022-07-08 11:12:28 -0400

    volume/brightness updates

--- brightness.sh
+++ brightness.sh
@@ -1,10 +1,37 @@
 #!/bin/bash
+brightness=100
 
-xrandr --output HDMI-1 --brightness 1 --output HDMI-0 --brightness 1
+BRIGHTNESS_FILE=/tmp/brightness-setting
 
-zenity --scale --text "Screen Brightness" --value=100 --print-partial | 
-  while IFS= read -r b
-  do
-    brightness=$(echo "$b * 0.01" | bc -l)
-    xrandr --output HDMI-1 --brightness $brightness --output HDMI-0 --brightness $brightness
-  done
+arg=${1:-'reset'}
+
+if [ -f $BRIGHTNESS_FILE ] ; then
+  brightness=$(cat $BRIGHTNESS_FILE)
+fi
+
+if [ $arg == "-" ] ; then
+  new_brightness=$(($brightness - 1))
+elif [ $arg == "+" ] ; then
+  new_brightness=$(($brightness + 1))
+elif [ $arg == "reset" ] ; then
+  new_brightness=100
+fi
+
+if [ $new_brightness -lt 1 ] ; then
+  new_brightness=1
+elif [ $new_brightness -gt 100 ] ; then
+  new_brightness=100
+fi
+
+echo -n $new_brightness > $BRIGHTNESS_FILE
+
+bright=$(bc -l <<< "$new_brightness * .01")
+
+xrandr --output HDMI-1 --brightness $bright --output HDMI-0 --brightness $bright
+
+ICON=display-brightness
+
+$HOME/.local/bin/notify-send.py "Brightness $new_brightness%" "" --expire-time 500 \
+                         --hint string:image-path:$ICON boolean:transient:true \
+                                int:has-percentage:$new_brightness \
+                         --replaces-process "brightness-popup"
