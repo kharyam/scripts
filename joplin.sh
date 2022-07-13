f82e390ef250c1346130f356b857e0483d1a4dd1
Khary Mendez <kharyam@gmail.com>
2022-07-13 14:28:19 -0400

    Joplin startup

--- joplin.sh
+++ joplin.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+
+pidof Joplin.AppImage
+if [ $? != 0 ] 
+  then
+  $HOME/.joplin/Joplin.AppImage &
+  $HOME/.local/bin/notify-send.py "Joplin" "Joplin is starting..." --expire-time 5000 
+fi
+
 xdotool key "Super_L+minus"
