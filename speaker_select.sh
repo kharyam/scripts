01bc285b5fb108bc9ca2413e0d6e048e2e4cf7e4
Khary Mendez <kharyam@gmail.com>
2022-11-02 17:41:04 -0400

    Wed Nov  2 05:41:04 PM EDT 2022

--- speaker_select.sh
+++ speaker_select.sh
@@ -5,7 +5,7 @@ if [ $1 ]
     SINK=$1
 else
   SINK=$(zenity --list --radiolist --hide-header --hide-column=3 --print-column=3 --text="Speaker Select" --column="Radio" --column="Friendly" --column="Actual" \
-	"" "Bluetooth" "bluez_output.E3_28_E9_23_20_79.a2dp-sink" \
+	"" "Bluetooth" "bluez_output.E3_28_E9_23_20_79.1" \
         "" "Headphones" "alsa_output.usb-C-Media_Electronics_Inc._USB_Audio_Device-00.iec958-stereo" \
         "" "Monitor" "alsa_output.pci-0000_01_00.1.hdmi-stereo")
 fi
