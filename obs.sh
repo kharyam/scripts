3bfd1aaae62f5773118c746c243b01d2c147c36d
Khary Mendez <kharyam@gmail.com>
2023-02-08 00:41:18 -0500

    Wed Feb  8 12:41:18 AM EST 2023

--- obs.sh
+++ obs.sh
@@ -15,5 +15,7 @@ if [ $? != 0 ]
    then
       nohup python3 -m http.server -d $HOME/Projects/local/webcam-browser 8087 &
       google-chrome --new-window --enable-features=OverlayScrollbar --profile-directory="Default" --app=http://localhost:8087
+      #brave-browser --new-window --enable-features=OverlayScrollbar --profile-directory="Default" --app=http://localhost:8087
       #vivaldi --new-window --profile-directory="Profile 1" --enable-features=OverlayScrollbar --app=http://localhost:8087
+      #firefox -P 'Red Hat' --kiosk --windowsize 1920,1080 http://localhost:8087
 fi
