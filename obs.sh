e388fecf934fbcffc4638a2b3bf333c0c4ae3e6f
Khary Mendez <kharyam@gmail.com>
2023-04-25 18:05:12 -0400

    Tue Apr 25 06:05:12 PM EDT 2023

--- obs.sh
+++ obs.sh
@@ -4,7 +4,7 @@ if [ $? != 0 ]
    then
       sudo rmmod v4l2loopback
       sudo modprobe v4l2loopback card_label="OBS"
-      export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib:/usr/lib64 
+      #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib:/usr/lib64 
       nohup /usr/local/bin/obs --startvirtualcam --scene "Camera" &
    sleep 15
 fi
@@ -14,8 +14,8 @@ xdotool search --name "^Display Webcam .*$"
 if [ $? != 0 ]
    then
       nohup python3 -m http.server -d $HOME/Projects/local/webcam-browser 8087 &
-      #google-chrome --new-window --enable-features=OverlayScrollbar --profile-directory="Default" --app=http://localhost:8087
       brave-browser --new-window --enable-features=OverlayScrollbar --profile-directory="Profile 2" --app=http://localhost:8087
+      #google-chrome --new-window --enable-features=OverlayScrollbar --profile-directory="Default" --app=http://localhost:8087
       #vivaldi --new-window --profile-directory="Profile 1" --enable-features=OverlayScrollbar --app=http://localhost:8087
       #firefox -P 'Red Hat' --kiosk --windowsize 1920,1080 http://localhost:8087
 fi
