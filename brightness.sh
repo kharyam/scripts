212f3bfeed40e7e0d3153d0cb7954f81ae2923b2
Khary Mendez <kharyam@gmail.com>
2023-06-01 17:58:19 -0400

    Thu Jun  1 05:58:19 PM EDT 2023

--- brightness.sh
+++ brightness.sh
@@ -30,7 +30,7 @@ echo -n $new_brightness > $BRIGHTNESS_FILE
 
 bright=$(bc -l <<< "$new_brightness * .01")
 
-xrandr --output HDMI-1 --brightness $bright --output DP-0 --brightness $bright
+xrandr --output HDMI-1 --brightness $bright --output DP-4 --brightness $bright
 
 ICON=display-brightness
 
