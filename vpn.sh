7367570448f7062c2dc7da2357009b2b39a34308
Khary Mendez <kharyam@gmail.com>
2023-09-19 22:33:16 -0400

    Tue Sep 19 10:33:16 PM EDT 2023

--- vpn.sh
+++ vpn.sh
@@ -4,5 +4,5 @@
 sleep 1 && xdotool search --name '^Authentication\ required$' windowactivate --sync type $(cat ~/.pin) &
 
 # Toggle vpn on or off
-nmcli connection up '1 Red-Hat-Global-VPN' || nmcli connection down '1 Red-Hat-Global-VPN'
+nmcli connection up '1 - Red Hat Global VPN' || nmcli connection down '1 - Red Hat Global VPN'
 
