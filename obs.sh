4e570b88775bd7f4a5c7cb31e3ce5abf84e7acc7
Khary Mendez <kharyam@gmail.com>
2024-05-16 21:32:50 -0400

    Thu May 16 09:32:50 PM EDT 2024

--- obs.sh
+++ obs.sh
@@ -11,13 +11,13 @@ if [ $? != 0 ]
 fi
 
 
-xdotool search --name "^Display Webcam .*$"
-if [ $? != 0 ]
-   then
-      sleep 10 
-      nohup python3 -m http.server -d $HOME/Projects/local/webcam-browser 8087 &
-      brave-browser --new-window --enable-features=OverlayScrollbar --profile-directory="Profile 2" --app=http://localhost:8087
-      #google-chrome --new-window --enable-features=OverlayScrollbar --profile-directory="Default" --app=http://localhost:8087
-      #vivaldi --new-window --profile-directory="Profile 1" --enable-features=OverlayScrollbar --app=http://localhost:8087
-      #firefox -P 'Red Hat' --kiosk --windowsize 1920,1080 http://localhost:8087
-fi
+#xdotool search --name "^Display Webcam .*$"
+#if [ $? != 0 ]
+#   then
+#      sleep 10 
+#      nohup python3 -m http.server -d $HOME/Projects/local/webcam-browser 8087 &
+#      brave-browser --new-window --enable-features=OverlayScrollbar --profile-directory="Profile 2" --app=http://localhost:8087
+#      #google-chrome --new-window --enable-features=OverlayScrollbar --profile-directory="Default" --app=http://localhost:8087
+#      #vivaldi --new-window --profile-directory="Profile 1" --enable-features=OverlayScrollbar --app=http://localhost:8087
+#      #firefox -P 'Red Hat' --kiosk --windowsize 1920,1080 http://localhost:8087
+#fi
