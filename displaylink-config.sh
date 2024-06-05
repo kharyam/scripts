5a42719ec456a6e302079fce9d330afd0c6dcbfa
Khary Mendez <kharyam@gmail.com>
2024-06-05 09:20:30 -0400

    Fix formatting

--- displaylink-config.sh
+++ displaylink-config.sh
@@ -2,5 +2,5 @@
 
 sudo modinfo -n evdi /lib/modules/$(uname -r)/extra/evdi.ko.xz
 sudo unxz $(modinfo -n evdi)
-sudo /usr/src/kernels/$(uname -r)/scripts/sign-file sha256 ./MOK.priv \\n  ./MOK.der /lib/modules/$(uname -r)/extra/evdi.ko
+sudo /usr/src/kernels/$(uname -r)/scripts/sign-file sha256 ./MOK.priv ./MOK.der /lib/modules/$(uname -r)/extra/evdi.ko
 sudo xz -f /lib/modules/$(uname -r)/extra/evdi.ko
