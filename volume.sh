657e74389542c846e0983b63c81b3b5bf5e26522
Khary Mendez <kharyam@gmail.com>
2024-07-03 18:30:30 -0400

    Wed Jul  3 06:30:30 PM EDT 2024

--- volume.sh
+++ volume.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 #
 
+win_id=$(xdotool search --name "Volume")
+if [ $? != 0 ]; then
+  ~/bin/xvolume.sh &
+  sleep 1
+  win_id=$(xdotool search --name "Volume")
+fi
+
+xdotool windowmap $win_id
+
 if [ "$1" == "inc" ]; then
    amixer -q sset Master 5%+
 fi
@@ -16,32 +25,32 @@ fi
 VOLUME=$(pactl get-sink-volume @DEFAULT_SINK@ | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,' | head -1)
 
 if [ "$VOLUME" -ge 100 ]; then
-  echo "HERE"
   VOLUME=100
   amixer -q sset Master 100%
 fi
 
-MUTE=$(pactl get-sink-mute @DEFAULT_SINK@)
-SINK=$(pactl info | grep "Default Sink" | cut -d ' ' -f 3)
-if [ "$VOLUME" -le 20 ]; then
-    ICON=audio-volume-low
-else if [ "$VOLUME" -le 60 ]; then
-         ICON=audio-volume-medium
-     else 
-         ICON=audio-volume-high
-     fi
-fi
-if [ "$MUTE" == "Mute: yes" ]; then
-    ICON=audio-volume-muted
+MARKER_FILE=/tmp/unmapvolumewindow.txt
+if [ ! -f $MARKER_FILE ] ; then
+touch $MARKER_FILE
+(sleep 3 ; xdotool windowunmap $win_id ; rm $MARKER_FILE)&
 fi 
 
-
-NOTI_ID=$($HOME/.local/bin/notify-send.py "Volume $VOLUME%" "($SINK)" --expire-time 500 \
-                         --hint string:image-path:$ICON boolean:transient:true \
-                                int:has-percentage:$VOLUME ) 
-                         #--replaces-process "volume-popup")
-
-
-#NOTI_ID=$($HOME/.local/bin/notify-send.py "Volume $VOLUME%" "($SINK)" --expire-time 250 \
+#if [ "$VOLUME" -le 20 ]; then
+#    ICON=audio-volume-low
+#else if [ "$VOLUME" -le 60 ]; then
+#         ICON=audio-volume-medium
+#     else 
+#         ICON=audio-volume-high
+#     fi
+#fi
+#MUTE=$(pactl get-sink-mute @DEFAULT_SINK@)
+#if [ "$MUTE" == "Mute: yes" ]; then
+#    ICON=audio-volume-muted
+#fi 
+#
+#SINK=$(pactl info | grep "Default Sink" | cut -d ' ' -f 3)
+#NOTI_ID=$($HOME/.local/bin/notify-send.py "Volume $VOLUME%" "($SINK)" --expire-time 500 \
 #                         --hint string:image-path:$ICON boolean:transient:true \
-#                                int:has-percentage:$VOLUME) 
+#                                int:has-percentage:$VOLUME ) 
+
+
