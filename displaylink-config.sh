d71fab4aebbfe17b8db13046ac9c023eaccc6d2f
Khary Mendez <kharyam@gmail.com>
2024-08-16 09:48:19 -0400

    Update swcursor to off

--- displaylink-config.sh
+++ displaylink-config.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo sed -i.bak 's/Option "SWCursor" "on"/Option "SWCursor" "off"/g' /etc/X11/xorg.conf.d/20-displaylink.conf
 sudo modinfo -n evdi /lib/modules/$(uname -r)/extra/evdi.ko.xz
 sudo unxz $(modinfo -n evdi)
 sudo /usr/src/kernels/$(uname -r)/scripts/sign-file sha256 ./MOK.priv ./MOK.der /lib/modules/$(uname -r)/extra/evdi.ko
