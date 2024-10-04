0ec3232b3acd17fd64a82e2efa576bcd59feec99
Khary Mendez <kharyam@gmail.com>
2024-10-04 00:41:33 -0400

    Specifiy path to kubeconfig

--- approve-csrs.sh
+++ approve-csrs.sh
@@ -1,1 +1,1 @@
-oc get csr --kubeconfig=kubeconfig-noingress  | grep Pending | awk '{print $1}' | xargs --no-run-if-empty oc adm certificate approve --kubeconfig=kubeconfig-noingress
+oc get csr --kubeconfig=kubeconfig-noingress  | grep Pending | awk '{print $1}' | xargs --no-run-if-empty oc adm certificate approve --kubeconfig=~/kubeconfig-noingress
