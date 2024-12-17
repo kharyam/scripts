0054ea6fa70a073fa2a93f617383a3896885671c
Khary Mendez <kharyam@gmail.com>
2024-12-17 08:29:57 -0500

    Docs for rebuilding displaylink rpm

--- displaylink-config.sh
+++ displaylink-config.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+## Run these steps to rebuild and install the rpm:
+# cd Projects/local/displaylink-rpm
+# git pull origin master
+# make github-release
+# find . -name "*.rpm"
+# sudo dnf install -y ./x86_64/displaylink-1.14.7-4.github_evdi.x86_64.rpm
+
 sudo sed -i.bak 's/Option "SWCursor" "on"/Option "SWCursor" "off"/g' /etc/X11/xorg.conf.d/20-displaylink.conf
 sudo modinfo -n evdi /lib/modules/$(uname -r)/extra/evdi.ko.xz
 sudo unxz $(modinfo -n evdi)
