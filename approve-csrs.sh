aaa4e43eaa8babae19aef12b8fcfe65f4206c7ae
Khary Mendez <kharyam@gmail.com>
2025-02-01 00:13:46 -0500

    Sat Feb  1 12:13:46 AM EST 2025

--- approve-csrs.sh
+++ approve-csrs.sh
@@ -1,1 +1,2 @@
-oc get csr --kubeconfig=${HOME}/kubeconfig-noingress  | grep Pending | awk '{print $1}' | xargs --no-run-if-empty oc adm certificate approve --kubeconfig=${HOME}/kubeconfig-noingress
+#oc get csr --kubeconfig=${HOME}/kubeconfig-noingress  | grep Pending | awk '{print $1}' | xargs --no-run-if-empty oc adm certificate approve --kubeconfig=${HOME}/kubeconfig-noingress
+oc get csr | grep Pending | awk '{print $1}' | xargs --no-run-if-empty oc adm certificate approve
