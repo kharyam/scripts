20f73861fde4f6f0985f20eb88c7328484da3137
Khary Mendez <kharyam@gmail.com>
2025-04-10 07:24:17 -0400

    Update light scripts

---
+++ adjust_lights.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+LIGHT_STATUS=$1
+SCREEN_BRIGHTNESS=${2:-1}
+
+DISPS=$(xrandr | grep -w connected | awk '{print $1}')
+
+for disp in $DISPS; do
+  xrandr --output $disp --brightness $SCREEN_BRIGHTNESS
+  ${HOME}/bin/lcli $LIGHT_STATUS
+done
--- obs_scene.sh
+++ obs_scene.sh
@@ -7,32 +7,27 @@ BACK_IN=4
 WHITE_BOARD=5
 NEWS=6
 
-DISP=$(xrandr | grep -w connected | awk '{print $1}')
+DISP=$(xrandr | grep -w connected | awk '{print $1}' | head -1)
 
-if [[ $1 == $WHITE_BOARD ]]
-  then
-    sleep 3
-    xrandr --output $DISP --brightness .3
-elif [[ $1 == $CAMERA ]]
-  then
-    xrandr --output $DISP --brightness .6
+if [[ $1 == $WHITE_BOARD ]]; then
+  sleep 3
+  xrandr --output $DISP --brightness .3
+elif [[ $1 == $CAMERA ]]; then
+  xrandr --output $DISP --brightness .6
 else
-    xrandr --output $DISP --brightness 1
+  xrandr --output $DISP --brightness 1
 fi
 
-for i in {0..10}
-do
-  xdotool search --desktop $i --name '^OBS .+$' windowactivate --sync key alt+shift+$1 
-  if [ $? -eq 0 ]
-    then
-      echo "Found OBS at desktop $i"
-      break
+for i in {0..10}; do
+  xdotool search --desktop $i --name '^OBS .+$' windowactivate --sync key alt+shift+$1
+  if [ $? -eq 0 ]; then
+    echo "Found OBS at desktop $i"
+    break
   fi
 done
 
-if [[ $1 == @($CAMERA|$SCREEN_SHARE|$NEWS) ]]
-  then
-    ${HOME}/bin/lcli on
-  else
-    ${HOME}/bin/lcli off
+if [[ $1 == @($CAMERA|$SCREEN_SHARE|$NEWS) ]]; then
+  ${HOME}/bin/lcli on
+else
+  ${HOME}/bin/lcli off
 fi
